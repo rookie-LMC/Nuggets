8fdfc7afc660f513dba1614587aeb8c0bbb4cebc
limengchen03 <limengchen03@kuaishou.com>
2024-10-27 19:44:52 +0800

    s3

---
+++ stock_3/save_stock_info_hk.py
@@ -0,0 +1,63 @@
+'''
+akshare API
+https://akshare.akfamily.xyz/data/stock/stock.html
+
+简单选股脚本
+https://blog.csdn.net/qq_46363011/article/details/128764258
+'''
+
+import time
+import akshare as ak
+import numpy as np
+from numpy import empty
+import pandas as pd
+import datetime as dt
+
+# 处理时间
+from dateutil.parser import parse
+from datetime import datetime, timedelta
+from chinese_calendar import is_workday, is_holiday
+
+from utils_stock import *
+
+'''
+全局参数
+'''
+debug_num = 200000000
+sleep_time_day_week_month_info = 0.3
+action_date = dt.date.today()
+start_date = '20220101'
+
+'''
+01 港股通成份股
+'''
+stock_list = ak.stock_hk_ggt_components_em()
+stock_symbol = stock_list['代码'].values
+stock_name = stock_list['名称'].values
+stocks_code = list(zip(stock_symbol, stock_name))
+# export_A_stocks(stock_list, action_date)
+print('**** 01 股票共计:', len(stocks_code))
+# print(stocks_code)
+
+'''
+03 召回
+个股日线、周线、月线
+'''
+print('*' * 50 + ' 03 召回数据')
+stock_daily, stock_weekly, stock_monthly = {}, {}, {}
+for i in range(min(len(stocks_code), debug_num)):
+    try:
+        print('**** load K line : ', stocks_code[i][0])
+        stock_daily[stocks_code[i][0]] = ak.stock_hk_hist(stocks_code[i][0], adjust="qfq", period="daily",
+                                                            start_date='20220101')
+        stock_weekly[stocks_code[i][0]] = ak.stock_hk_hist(stocks_code[i][0], adjust="qfq", period="weekly",
+                                                             start_date='20220101')
+        stock_monthly[stocks_code[i][0]] = ak.stock_hk_hist(stocks_code[i][0], adjust="qfq", period="monthly",
+                                                              start_date='20220101')
+        export_hk_stocks(stock_daily[stocks_code[i][0]], stock_weekly[stocks_code[i][0]],
+                        stock_monthly[stocks_code[i][0]],
+                        action_date, stocks_code[i][0])
+        time.sleep(sleep_time_day_week_month_info)
+    except:
+        print('**** has no day week month K line: ', stocks_code[i][0])
+print('*' * 50 + ' 03 召回数据完毕')
--- stock_3/utils_stock.py
+++ stock_3/utils_stock.py
@@ -13,7 +13,23 @@ def export_A_stocks(stock_daily, stock_weekly, stock_monthly, action_date, stock
         stock_monthly.to_excel(writer, sheet_name='monthly')
 
 
+def export_hk_stocks(stock_daily, stock_weekly, stock_monthly, action_date, stock_code):
+    file_path = './stock_info_hk' + '/' + str(action_date) + '-' + str(stock_code) + '.xlsx'
+    print(file_path)
+    # stock_date.to_excel(file_path, sheet_name=sheet_name)
 
-        # export_A_stocks(stock_daily[stocks_code[i][0]], stock_weekly[stocks_code[i][0]],
-        #                 stock_monthly[stocks_code[i][0]],
-        #                 action_date, stocks_code[i][0])
+    with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
+        stock_daily.to_excel(writer, sheet_name='daily')
+        stock_weekly.to_excel(writer, sheet_name='weekly')
+        stock_monthly.to_excel(writer, sheet_name='monthly')
+
+
+def load_stocks(action_date, stock_code, field='A'):
+    file_path = './stock_info' + '/' + str(action_date) + '-' + str(stock_code) + '.xlsx'
+    if field == 'hk':
+        file_path = './stock_info_hk' + '/' + str(action_date) + '-' + str(stock_code) + '.xlsx'
+    daily = pd.read_excel(file_path, sheet_name='daily')
+    weekly = pd.read_excel(file_path, sheet_name='weekly')
+    monthly = pd.read_excel(file_path, sheet_name='monthly')
+
+    return daily, weekly, monthly
